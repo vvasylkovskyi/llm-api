a494b5a9ac95deebc0574e9a9d0094c4f2edde39
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2026-05-31 22:28:56 +0100

    Add Database Connection check

---
+++ llm_api/database/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import text
+from sqlmodel import create_engine
+
+from llm_api.database.config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+
+def ping_db() -> bool:
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT 1"))
+        return result.scalar() == 1
